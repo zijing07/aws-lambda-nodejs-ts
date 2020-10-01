5915fe2c2a4c0a5ddb652858148c72641b99a48c
Zijing Zhao <zijzha@amazon.com>
2020-09-30 22:11:56 -0700

    add unit test

---
+++ hello-world/src-ts/tests/unit/test-handler.test.ts
@@ -0,0 +1,16 @@
+import { APIGatewayProxyEvent } from "aws-lambda";
+import { lambdaHandler } from "../../app";
+
+describe('Tests index', function () {
+    it('verifies successful response', async () => {
+        const event: APIGatewayProxyEvent = {
+            queryStringParameters: {
+                a: "1"
+            }
+        } as any
+        const result = await lambdaHandler(event)
+
+        expect(result.statusCode).toEqual(200);
+        expect(result.body).toEqual(`Queries: ${JSON.stringify(event.queryStringParameters)}`);
+    });
+});
